ab03bd2edfde663199bd91f584247ca7b1ce0ac1
drewbyron <william.andrew.byron@gmail.com>
2022-11-15 08:30:42 -0800

    Making the experiment name default to the .txt file name.

--- he6_cres_spec_sims/run_rocks_experiment.py
+++ he6_cres_spec_sims/run_rocks_experiment.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import argparse
+from pathlib import Path
 
 # # Path to local imports.
 # sys.path.append("/home/drew/He6CRES/he6-cres-spec-sims/")
@@ -39,6 +40,9 @@ def run_experiment(dict_path):
     for key, val in sim_experiment_params.items():
         print("{}: {}".format(key, val))
 
+    # Make the experiment name match the name of the .txt file. 
+    experiment_name = Path(dict_path).stem
+    sim_experiment_params["experiment_name"] = experiment_name
     sim_experiment = exp.RocksExperiment(sim_experiment_params)
 
     print(f"\n\n\n Done running simulation. {sim_experiment_params}")
