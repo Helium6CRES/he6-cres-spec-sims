817e515d9332a4fc6ff4c8d489d5f0b5e9b8f1f4
drewbyron <william.andrew.byron@gmail.com>
2022-11-16 17:11:29 -0800

    Pushing changes to what gets written. Only writing dm tracks now.

--- he6_cres_spec_sims/spec_tools/spec_calc/power_calc.py
+++ he6_cres_spec_sims/spec_tools/spec_calc/power_calc.py
@@ -3,7 +3,8 @@ import scipy.special as sp
 from scipy.integrate import simps
 import math
 
-import spec_calc as sc
+import spec_tools.spec_calc.spec_calc as sc
+
 
 def rho_phi(center_x, center_y, time, frequency, field):
     """
